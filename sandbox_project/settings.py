d31fca5ed497af7776b202575781ac614d88f356
A-NEUN <a.neun@outlook.com>
2020-02-08 12:30:21 +0000

    Added heroku to allowed hosts

--- sandbox_project/settings.py
+++ sandbox_project/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = '43)%4yx)aa@a=+_c(fn&kf3g29xax+=+a&key9i=!98zyim=8j'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'tools.wmflabs.org',
+    'localhost',
+    'gentle-bastion-55000.herokuapp.com',
 ]
 
 
